ee15b18fca6b36dae036df752c4963dfa55648b0
mgoldm <57058926+mgoldm@users.noreply.github.com>
2022-05-28 12:38:05 +0300

    Update request.rb

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,8 +3,16 @@ class Request < ApplicationRecord
 
   has_many :responses, dependent: :destroy
   has_many :key_words, dependent: :destroy
+  
+  after_create :create_search
 
   accepts_nested_attributes_for :key_words, reject_if: :all_blank
 
   validates :body, presence: true
+  
+  private
+  
+  def create_search
+    ParsingJob.perform_later(self)
+  end
 end
